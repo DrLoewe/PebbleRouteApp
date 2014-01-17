e288ba87f16d360d775ca381493953d32fc9da20
Remus Lazar <remus@lazar.info>
2014-01-17 18:50:17 +0100

    calculate the current step on the current route using all the points from the route polyline

--- PebbleRoute.m
+++ PebbleRoute.m
@@ -17,13 +17,6 @@
 
 - (MKPolyline *)currentRoutePath
 {
-	NSUInteger indexOfCurrentStep = [self.route.steps indexOfObject:self.currentStep];
-	if (indexOfCurrentStep != NSNotFound) {
-		if (self.route.steps.count > (indexOfCurrentStep+1)) {
-			MKRouteStep *step = self.route.steps[indexOfCurrentStep+1];
-			if (step) return step.polyline;
-		}
-	}
 	return self.currentStep.polyline;
 }
 
@@ -32,6 +25,7 @@
 	_route = route;
 	self.currentStep = [self.route.steps firstObject];
 	[self calculateDistance];
+    [self calculateCurrentStep];
 }
 
 // get the distance (in m) from a specified location to self.location
@@ -67,16 +61,36 @@
 - (void)setLocation:(CLLocation *)location
 {
 	_location = location;
-	
-	NSUInteger currentDistance = [self distanceFrom:self.currentStep.polyline.coordinate];
-	
-	for (MKRouteStep *step in self.route.steps) {
-		int distance = [self distanceFrom:step.polyline.coordinate];
-		if (distance < currentDistance) {
-			self.currentStep = step;
-		}
-	}
+    [self calculateCurrentStep];
 	[self calculateDistance];
+}
+
+// calculate the minimum distance from the current location to a given routeStep
+- (void)calculateCurrentStep
+{
+    MKMapPoint origin = MKMapPointForCoordinate(self.location.coordinate);
+    float minDistance = MAXFLOAT;
+    MKMapPoint pointOnPath;
+    MKRouteStep *currentStep;
+
+    for (MKRouteStep *routeStep in self.route.steps) {
+        for (NSUInteger i=0; i<routeStep.polyline.pointCount; i++) {
+            MKMapPoint point = routeStep.polyline.points[i];
+            float dx = point.x - origin.x;
+            float dy = point.y - origin.y;
+            float distance = dx * dx + dy * dy;
+            if (distance <= minDistance) {
+                minDistance = distance;
+                pointOnPath = point;
+                currentStep = routeStep;
+            }
+        }
+    }
+
+//    CLLocationCoordinate2D coordOnRoute = MKCoordinateForMapPoint(pointOnPath);
+//    float distance = [self.location distanceFromLocation:[[CLLocation alloc] initWithLatitude:coordOnRoute.latitude longitude:coordOnRoute.longitude]];
+//    NSLog(@"distance to route: %.1fm", distance);
+    self.currentStep = currentStep;
 }
 
 @end
